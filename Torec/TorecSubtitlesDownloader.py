2212c622a9f8e93346a69aa604e321c6b6743577
slmosl <slmosl@gmail.com>
2012-12-01 22:41:04 +0200

    Recontended with Torec changes
    Torec has made some changes that allowed them to identify the script
    working and downloaded fishy subtitles. Not any longer

--- Torec/TorecSubtitlesDownloader.py
+++ Torec/TorecSubtitlesDownloader.py
@@ -1,7 +1,8 @@
 import cookielib
+import datetime
 import zipfile
-import urllib
 import urllib2
+import urllib
 import time
 import os
 import sys
@@ -20,9 +21,10 @@ class SubtitleOption(object):
         return "%s" % (self.name)
     
 class SubtitlePage(object):
-    def __init__(self, id, name, data):
+    def __init__(self, id, name, url, data):
         self.id = id
         self.name = name
+        self.url = url
         self.options = self._parseOptions(data)
         
     def _parseOptions(self, data):
@@ -35,6 +37,20 @@ class SubtitlePage(object):
         for option in self.options:
             log(__name__ ,option)
         
+class Response(object):
+    def __init__(self, response):
+        self.data = self._handleData(response)
+        self.headers = response.headers
+        
+    def _handleData(self, resp):
+        data = resp.read()
+        if (len(data) != 0):
+            try:
+                data = zlib.decompress(data, 16+zlib.MAX_WBITS)
+            except zlib.error:
+                pass
+        return data
+
 class FirefoxURLHandler():
     def __init__(self):
         cj = cookielib.CookieJar()
@@ -42,43 +58,48 @@ class FirefoxURLHandler():
         self.opener.addheaders = [('Accept-Encoding','gzip, deflate'),
                                   ('Accept-Language', 'en-us,en;q=0.5'),
                                   ('Pragma', 'no-cache'),
-                                  ('Cache-Control', 'no-cache')]
+                                  ('Cache-Control', 'no-cache'),
+                                  ('User-Agent', 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0')]
     
-    def request(self, url, data=None, ajax=False):
+    def request(self, url, data=None, ajax=False, referer=None, cookie=None):
         if (data != None):
             data = urllib.urlencode(data)
+        # FIXME: Awful code duplication
         if (ajax == True):
             self.opener.addheaders += [('X-Requested-With', 'XMLHttpRequest')]
+        if (referer != None):
+            self.opener.addheaders += [('Referer', referer)]
+        if (cookie != None):
+            self.opener.addheaders += [('Cookie', cookie)]
   
-        resp = self.opener.open(url, data)
-        data = resp.read()
-        
-        if (len(data) != 0):
-            try:
-                data = zlib.decompress(data, 16+zlib.MAX_WBITS)
-            except zlib.error:
-                pass
-        
-        return (data, resp.headers)
-    
+        resp = self.opener.open(url, data)        
+        return Response(resp)
 
 class TorecSubtitlesDownloader:
     DEFAULT_SEPERATOR = " "
     BASE_URL = "http://www.torec.net"
     SUBTITLE_PATH = "sub.asp?sub_id="
+    DEFAULT_COOKIE = "Torec_NC_s=%(screen_width)d; Torec_NC_sub_%(subId)s=sub=%(current_datetime)s"
 
     def __init__(self):
         self.urlHandler = FirefoxURLHandler()
         
+    def _buildDefaultCookie(self, subID):
+        currentTime = datetime.datetime.now().strftime("%m/%d/%Y+%I:%M:%S+%p")
+        return self.DEFAULT_COOKIE % {"screen_width" : 1760, 
+                                      "subId" : subID, 
+                                      "current_datetime" : currentTime}
+    
     def searchMovieName(self, movieName):
-        data = self.urlHandler.request("%s/ssearch.asp" % self.BASE_URL, {"search" : movieName})[0]
-        match = re.search('sub\.asp\?sub_id=(\w+)', data)
+        response = self.urlHandler.request("%s/ssearch.asp" % self.BASE_URL, {"search" : movieName})
+        match = re.search('sub\.asp\?sub_id=(\w+)', response.data)
         if (match is None):
             return None
           
         id = match.groups()[0]
-        subtitleData = self.urlHandler.request("%s/%s%s" % (self.BASE_URL, self.SUBTITLE_PATH, id))[0]
-        return SubtitlePage(id, movieName, subtitleData)
+        subURL = "%s/%s%s" % (self.BASE_URL, self.SUBTITLE_PATH, id)
+        subtitleData = self.urlHandler.request(subURL).data
+        return SubtitlePage(id, movieName, subURL, subtitleData)
         
     def findChosenOption(self, name, subtitlePage):
         name = name.split(self.DEFAULT_SEPERATOR)
@@ -97,27 +118,29 @@ class TorecSubtitlesDownloader:
 
         return chosenOption
         
-    def _requestSubtitle(self, subID):
-        params = {"sub_id" : subID}
-        return self.urlHandler.request("%s/ajax/sub/guest_time.asp" % self.BASE_URL, params, ajax=True)[0]
+    def _requestSubtitle(self, subID, subURL):
+        params = {"sub_id"  : subID, 
+                  "s"       : 1760}
+                  
+        return self.urlHandler.request("%s/ajax/sub/guest_time.asp" % self.BASE_URL, params, 
+                                        ajax=True, referer=subURL, cookie=self._buildDefaultCookie(subID)).data
         
-    def getDownloadLink(self, subID, optionID, persist=True):        
-        requestID = self._requestSubtitle(subID)
+    def getDownloadLink(self, subID, optionID, subURL, persist=True):        
+        requestID = self._requestSubtitle(subID, subURL)
         
         params = {"sub_id" : subID, "code": optionID, "sh" : "yes", "guest" : requestID, "timewaited" : "16"}
-
         for i in xrange(16):
-            data = self.urlHandler.request("%s/ajax/sub/download.asp" % self.BASE_URL, params, ajax=True)[0]
-            if (len(data) != 0 or not persist):
+            response = self.urlHandler.request("%s/ajax/sub/download.asp" % self.BASE_URL, params, ajax=True)
+            if (len(response.data) != 0 or not persist):
                 break
             time.sleep(1)
             
-        return (data)
+        return response.data
         
     def download(self, downloadLink):
-        (data, headers) = self.urlHandler.request("%s%s" % (self.BASE_URL, downloadLink))
-        fileName = re.search("filename=(.*)", headers["content-disposition"]).groups()[0]
-        return (data, fileName)
+        response = self.urlHandler.request("%s%s" % (self.BASE_URL, downloadLink))
+        fileName = re.search("filename=(.*)", response.headers["content-disposition"]).groups()[0]
+        return (response.data, fileName)
         
     def saveData(self, fileName, data, shouldUnzip=True):
         log(__name__ ,"Saving to %s (size %d)" % (fileName, len(data)))
@@ -139,37 +162,37 @@ class TorecSubtitlesDownloader:
     def getSubtitleMetaData(self, movieName):
         sanitizedName = self.sanitize(movieName)
         log(__name__ , "Searching for %s" % sanitizedName)
-        susbtitlePage = self.searchMovieName(sanitizedName)
-        if susbtitlePage is None:
+        subtitlePage = self.searchMovieName(sanitizedName)
+        if subtitlePage is None:
             log(__name__ ,"Couldn't find relevant subtitle page")
             return
             
         log(__name__ , "Found relevant meta data")
-        return susbtitlePage
+        return subtitlePage
         
     def getSubtitleData(self, movieName, resultSubtitleDirectory):
-        susbtitlePage = self.getSubtitleMetaData(movieName)
+        subtitlePage = self.getSubtitleMetaData(movieName)
         # Try to choose the most relevant option according to the file name
-        chosenOption = self.findChosenOption(susbtitlePage.name, susbtitlePage)
+        chosenOption = self.findChosenOption(subtitlePage.name, subtitlePage)
         if chosenOption != None:
             log(__name__ ,"Found the subtitle type - %s" % chosenOption)
         else:
             
             log(__name__ ,"No suitable subtitle found!")
             log(__name__ ,"Available options are:")
-            options = enumerate(susbtitlePage.options, start=1)
+            options = enumerate(subtitlePage.options, start=1)
             for num, option in options:
                 log(__name__ ,"\t(%d) %s" % (num, option))
                 
             choice = int(raw_input("What subtitle do you want to download? "))
-            while (choice < 0 or choice > len(susbtitlePage.options)):
+            while (choice < 0 or choice > len(subtitlePage.options)):
                 log(__name__ ,"bad choice")
                 choice = int(raw_input("What subtitle do you want to download? "))
         
-            chosenOption = susbtitlePage.options[choice-1]
+            chosenOption = subtitlePage.options[choice-1]
 
         # Retrieve the download link and download the subtitle
-        downloadLink = self.getDownloadLink(susbtitlePage.id, chosenOption.id)
+        downloadLink = self.getDownloadLink(subtitlePage.id, chosenOption.id, subtitlePage.url)
         if (downloadLink == ""):
             log(__name__ ,"Download Unsuccessful!")
             return
